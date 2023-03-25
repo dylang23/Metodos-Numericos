af8a6ed77fa1ce218a0e04503fe0a5e1b2ac08bd
Daniel Galarza <galarza.danielemanuel96@gmail.com>
2023-03-24 22:38:05 -0300

    Archivo de python para parsear expressiones

---
+++ parser/parsear_expression.py
@@ -0,0 +1,36 @@
+from sympy import Symbol, lambdify, Expr, sin
+from sympy.parsing.sympy_parser import parse_expr, standard_transformations
+from sympy.parsing.sympy_parser import implicit_multiplication, implicit_application, function_exponentiation
+
+
+def str_to_exprsympy(expression: str) -> Expr:
+    
+    x = Symbol('x')
+
+    local_dict = {'x': x, 'sen': sin}
+    
+    transformations = standard_transformations + \
+        (implicit_multiplication, implicit_application, function_exponentiation)
+
+    expression_sympy = parse_expr(expression, local_dict=local_dict, transformations=transformations)
+
+    return expression_sympy
+
+
+def exprsympy_to_function(expression_sympy: Expr) -> callable:
+    
+    x = Symbol('x')
+    
+    function = lambdify(x, expression_sympy, "numpy")
+
+    return function
+
+
+def str_to_function(expression: str) -> callable:
+    
+    expr_sympy = str_to_exprsympy(expression)
+    
+    function = exprsympy_to_function(expr_sympy)
+
+    return function
+
