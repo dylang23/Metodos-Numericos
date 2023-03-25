6a011c50414ffe05dc12079fe8866c776977b985
Daniel Galarza <galarza.danielemanuel96@gmail.com>
2023-03-25 09:55:33 -0300

    nuevos test para metodos cerrados

--- test/test_metodos_cerrados.py
+++ test/test_metodos_cerrados.py
@@ -1,2 +1,8 @@
-from parser import str_to_function
+from parsers.parsear_expression import str_to_function
+from algoritmos_numericos.raices_ecuaciones.metodos_cerrados import biseccion
+
+expression = "cos(4x-2) + exp(1-x)"
+funcion = str_to_function(expression)
+
+raiz = biseccion(funcion, 0.8, 1.2, error_aceptado=0.00001)
 
